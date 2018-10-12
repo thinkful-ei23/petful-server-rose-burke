5255400f48fc79fc5bf4198f1a75fa4e785cffed
Burkey Koontz <bk.koontz@gmail.com>
2018-10-12 08:29:07 -0700

    added cat endpoint

--- index.js
+++ index.js
@@ -22,6 +22,24 @@ app.use(
   })
 );
 
+app.get('/api/cat', (req, res, next) => {
+  const cat = {
+    imageURL:'https://assets3.thrillist.com/v1/image/2622128/size/tmg-slideshow_l.jpg', 
+    imageDescription: 'Orange bengal cat with black stripes lounging on concrete.',
+    name: 'Fluffy',
+    sex: 'Female',
+    age: 2,
+    breed: 'Bengal',
+    story: 'Thrown on the street'
+  };
+
+  res.send(cat)
+    .catch(err => {
+      next(err);
+    });
+});
+
+
 function runServer(port = PORT) {
   const server = app
     .listen(port, () => {
